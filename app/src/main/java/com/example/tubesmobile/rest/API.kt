2636fff4b4f6bf6520b02b3bb5ff4602d3223836
Imamh28 <ggmukmin@gmail.com>
2023-12-18 13:21:22 +0700

    API, retrofit

---
+++ app/src/main/java/com/example/tubesmobile/rest/API.kt
@@ -0,0 +1,40 @@
+package com.example.tubesmobile.rest
+
+import com.example.tubesmobile.data.*
+import retrofit2.Call
+import retrofit2.http.*
+
+interface API {
+    @GET("read.php")
+    fun getCountries():Call<ArrayList<CountryItem>>
+
+    @GET("detail.php")
+    fun getCountryDetail(
+        @Query("countryId") countryId: String?
+    ):Call<CountryDetail>
+
+    @FormUrlEncoded
+    @POST("add.php")
+    fun addCountryDetail(
+        @Field("country_name") country_name: String?,
+        @Field("country_continent") country_continent: String?,
+        @Field("country_population") country_population: String?,
+        @Field("country_description") country_description: String?
+    ): Call<Response>
+
+    @FormUrlEncoded
+    @POST("update.php")
+    fun updateCountryDetail(
+        @Field("country_id") country_id: String?,
+        @Field("country_name") country_name: String?,
+        @Field("country_continent") country_continent: String?,
+        @Field("country_population") country_population: String?,
+        @Field("country_description") country_description: String?
+    ): Call<Response>
+
+    @FormUrlEncoded
+    @POST("delete.php")
+    fun deleteCountryDetail(
+        @Field("country_id") country_id: String?
+    ): Call<Response>
+}
